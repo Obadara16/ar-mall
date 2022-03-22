429aa12ce65407a384dcfaed7cd30b5921ade5d7
Roqeeb Obadara <obadararoqeeb@gmail.com>
2022-03-22 15:13:44 +0100

    first commit on my react app

---
+++ src/data.js
@@ -0,0 +1,73 @@
+export const sliderItems = [{
+        id: 1,
+        img: "https://media.istockphoto.com/photos/couple-in-shopping-picture-id811400420?k=20&m=811400420&s=612x612&w=0&h=OBabxS6hrSF8ODiCAHgmxAI6NqoszpdZ-zxvlJyoaWA=",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://pixabay.com/get/g1397591846b36a1d844681d1a63f4fa15743b0baa223098a73829194d3857c8431191aeba3bdd1a7e32d7307bb658cb1682bcdae1ae63b69fdf7c5b58d03ae3234d95e8e9d56daf6952794d659ec1a81_1920.jpg",
+        title: "ADIDAS BRAND DAY",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://pixabay.com/get/g78976c1bde7ed3e211987ee4da17370fa35a3a02f8e4af3aa3c849409fb596f752ee0ea1a126e7e492d636d1aee7bdb9eb422fab39b6e8987f2d9e68e99ce55d6ed0ec9272fc6893a93f87375a26def4_1920.jpgLLV/3.png",
+        title: "TECH WEEK",
+        desc: "THE TECH SALE YOU CAN'T MISS",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [{
+        id: 1,
+        img: "https://pixabay.com/get/g9eeff60219fc480ee5b42501aa19760c4c777233352de37c2c447bf70e8c48dc45a5bcee8973970e8e2f6ea337bdf0bcbad093af17a9bd8c415a65d710f99215ee31e97aa8d89d7b9d82905ea4149e74_1920.jpg",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://pixabay.com/get/g2cdda33f71dede265f339869a7a083a2603f9ae9df7914fd3d60838d39a8c3c78878de67a650ff91202caa7d90bb65033b5102bdf7eb25407e432ac2c57ed411b8107741b19a7dd791df34df380fdcf1_1920.jpg",
+        title: "NURA HEADSET",
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        title: "LIGHT JACKETS",
+    },
+];
+
+export const popularProducts = [{
+        id: 1,
+        img: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/74/500626/1.jpg?7638",
+    },
+    {
+        id: 2,
+        img: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/08/544099/1.jpg?9285",
+    },
+    {
+        id: 3,
+        img: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/83/736599/1.jpg?1050",
+    },
+    {
+        id: 4,
+        img: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/92/774107/1.jpg?3071",
+    },
+    {
+        id: 5,
+        img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id: 6,
+        img: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/06/898537/1.jpg?6853",
+    },
+    {
+        id: 7,
+        img: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/55/553962/1.jpg?5833",
+    },
+    {
+        id: 8,
+        img: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/58/073128/1.jpg?7281",
+    },
+]
